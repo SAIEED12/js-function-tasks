aef552f543e739cb302c5364e5d41735d229cfd8
SAIEED12 <abusaieed0@gmail.com>
2026-01-31 20:30:00 +0600

    first task

---
+++ task1.js
@@ -0,0 +1,7 @@
+/* Take four parameters. Multiply the four numbers and then return the result */
+
+function multiply(num1,num2,num3,num4){
+    const mult = num1*num2*num3*num4;
+    return mult;
+}
+console.log("Result is:",multiply(10,20,30,40));
